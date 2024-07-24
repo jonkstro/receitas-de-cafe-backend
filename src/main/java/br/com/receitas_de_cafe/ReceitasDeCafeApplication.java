5ecedc7061954aed6e3fb866fb32352f0e997b3f
Jonas <jonascastro.dev@gmail.com>
2024-07-24 19:11:13 -0300

    Primeiro commit

---
+++ src/main/java/br/com/receitas_de_cafe/ReceitasDeCafeApplication.java
@@ -0,0 +1,13 @@
+package br.com.receitas_de_cafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReceitasDeCafeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReceitasDeCafeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/receitas_de_cafe/ReceitasDeCafeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.receitas_de_cafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceitasDeCafeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
